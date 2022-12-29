b746a12ed1291bd02153e9119ca4b42529c2e59d
Vitor_Liaz88 <vnla.lic18@uea.edu.br>
2022-12-29 08:42:14 -0400

    chore: jest

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,16 @@
-/*
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
-
 module.exports = {
-  verbose: true,
   roots: ['<rootDir>/src'],
-  collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**'
   ],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
+  coverageProvider: 'babel',
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
-  preset: '@shelf/jest-mongodb',
   moduleNameMapper: {
     '@/tests/(.*)': '<rootDir>/tests/$1',
     '@/(.*)': '<rootDir>/src/$1'

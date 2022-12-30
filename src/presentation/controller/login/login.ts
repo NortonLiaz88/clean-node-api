7592766d5f9b630c0ecd9ce183943bb59a949c69
Vitor_Liaz88 <vnla.lic18@uea.edu.br>
2022-12-30 19:49:47 -0400

    refactor: code duplication

--- src/presentation/controller/login/login.ts
+++ src/presentation/controller/login/login.ts
@@ -1,7 +1,12 @@
 import { Authentication } from '../../../data/usecases/autentication/authentication'
 import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest, serverError } from '../../helpers/http-helpers'
-import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../../protocols'
+import {
+  Controller,
+  EmailValidator,
+  HttpRequest,
+  HttpResponse
+} from '../../protocols'
 
 export class LoginController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -13,33 +18,27 @@ export class LoginController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const requiredFields = ['email', 'password']
+      for (const field of requiredFields) {
+        if (!httpRequest.body(field)) {
+          throw new MissingParamError(field)
+        }
+      }
       const { email, password } = httpRequest.body
       // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-      if (!email) {
-        return await new Promise<HttpResponse>((resolve) =>
-          resolve(badRequest(new MissingParamError('email')))
-        )
+
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
       }
-      if (!password) {
-        return await new Promise<HttpResponse>((resolve) =>
-          resolve(badRequest(new MissingParamError('password')))
-        )
-      } else {
-        const isValid = this.emailValidator.isValid(email)
-        if (!isValid) {
-          return await new Promise<HttpResponse>((resolve) =>
-            resolve(badRequest(new InvalidParamError('email')))
-          )
+      const token = await this.authentication.auth(email, password)
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          token
         }
-        const token = await this.authentication.auth(email, password)
-        const httpResponse: HttpResponse = {
-          statusCode: 200,
-          body: {
-            token
-          }
-        }
-        return httpResponse
       }
+      return httpResponse
     } catch (error) {
       return serverError(error)
     }

c408e593799faa8da51d215d44742dac96670c2b
Vitor_Liaz88 <vnla.lic18@uea.edu.br>
2023-01-07 13:34:10 -0400

    test: add to compare field validation

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec .ts
@@ -0,0 +1,16 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-field-validation'
+
+describe('RequiredField Validation', () => {
+  test('should return a Missing Param error if  validation fails', () => {
+    const sut = new CompareFieldsValidation('field', 'fiedlToCompare')
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'wrong_value' })
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+
+  test('should not return a Missing Param error if  validation succeeds', () => {
+    const sut = new CompareFieldsValidation('field', 'fiedlToCompare')
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'any_value' })
+    expect(error).toBeFalsy()
+  })
+})

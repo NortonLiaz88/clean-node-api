adceac188315c9ecdd33f4d684f8f5a89e09455b
Vitor_Liaz88 <vnla.lic18@uea.edu.br>
2023-01-17 13:40:54 -0400

    feat: ensure SurveyMongoRepository load all surveys

--- src/data/usecases/load-surveys/db-load-surveys .spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys .spec.ts
@@ -13,17 +13,48 @@ const makeFakeSurveys = (): SurveyModel[] => {
   ]
 }
 
+interface SutTypes {
+  sut: DbLoadSurveys
+  loadSurveysRepositoryStub: LoadSurveysRepository
+}
+
+const makeLoadSurveysRepository = (): LoadSurveysRepository => {
+  class LoadSurveysRepositoryStub implements LoadSurveysRepository {
+    async loadAll (): Promise<SurveyModel[]> {
+      return await new Promise<SurveyModel[]>((resolve, reject) => resolve(makeFakeSurveys()))
+    }
+  }
+
+  return new LoadSurveysRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveysRepositoryStub = makeLoadSurveysRepository()
+  const sut = new DbLoadSurveys(loadSurveysRepositoryStub)
+  return {
+    sut,
+    loadSurveysRepositoryStub
+  }
+}
+
 describe('DbLoadSurveys', () => {
   test('should call LoadSurveysRepository', async () => {
-    class LoadSurveysRepositoryStub implements LoadSurveysRepository {
-      async loadAll (): Promise<SurveyModel[]> {
-        return await new Promise<SurveyModel[]>((resolve, reject) => resolve(makeFakeSurveys()))
-      }
-    }
-    const loadSurveysRepositoryStub = new LoadSurveysRepositoryStub()
+    const { sut, loadSurveysRepositoryStub } = makeSut()
     const loadAllSpy = jest.spyOn(loadSurveysRepositoryStub, 'loadAll')
-    const sut = new DbLoadSurveys(loadSurveysRepositoryStub)
     await sut.load()
     expect(loadAllSpy).toHaveBeenCalled()
+  })
+
+  test('should return a list of Surveys on success', async () => {
+    const { sut } = makeSut()
+    const surveys = await sut.load()
+    expect(surveys).toEqual(makeFakeSurveys())
+  })
+
+  test('should throw if LoadAaccountByTokenRepositoy throws', async () => {
+    const { sut, loadSurveysRepositoryStub } = makeSut()
+    jest.spyOn(loadSurveysRepositoryStub, 'loadAll').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.load()
+    await expect(promise).rejects.toThrow()
   })
 })
--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,18 @@
+import { LoadSurveysRepository } from './../../../../data/protocols/db/survey/load-surveys-repository'
 import { AddSurveyRepository } from './../../../../data/protocols/db/survey/add-survey-reository'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AddSurveyModel } from '../../../../domain/usecase/add-survey'
+import { SurveyModel } from '../../../../domain/models/survey'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add (data: AddSurveyModel): Promise<void> {
     const accoutCollection = await MongoHelper.getCollection('surveys')
     await accoutCollection.insertOne({ ...data })
+  }
+
+  async loadAll (): Promise<SurveyModel[]> {
+    const surveysCollection = await MongoHelper.getCollection('surveys')
+    const surveys: SurveyModel[] = await surveysCollection.find().toArray()
+    return surveys
   }
 }
